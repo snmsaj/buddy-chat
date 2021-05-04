8f72ee2fcdafb166b2479ce04c69fdf3191f1e2f
Shawn Saj <76454973+snmsaj@users.noreply.github.com>
2021-05-04 17:39:25 -0400

    Completed registration and functionality

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,7 +1,105 @@
+import { Grid, Form, Segment, Button, Header, Message } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import firebase from '../../firebase';
+import md5 from 'md5';
+
 const Register = () => {
-    return ( 
-        <div>Register</div>
-     );
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirmation, setPasswordConfirmation] = useState('');
+    const [error, setError] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const usersRef = firebase.database().ref('users')
+
+    const isPasswordValid = () => {
+        if (password.length < 6 || passwordConfirmation.length < 6) {
+            return false;
+        }else if (password !== passwordConfirmation) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // if (password !== passwordConfirmation) {
+        //     return setError('Passwords do not match')
+        // }
+
+        if (isPasswordValid()) {
+            setError('')
+            setIsLoading(true)
+
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(email, password)
+                .then(createdUser => {
+                    console.log(createdUser)
+                    setError('')
+                    setIsLoading(false)
+                    createdUser.user.updateProfile({
+                        displayName: username,
+                        photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon`
+                    })
+                    .then(() => {
+                        // setIsLoading(false)
+                        saveUser(createdUser).then(() => {
+                            console.log('user saved')
+                        })
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        setError(`${err}`)
+                        setIsLoading(false)
+                    })
+                })
+                .catch(err => {
+                    console.error(err)
+                    setError(`${err}`)
+                    setIsLoading(false)
+                })
+        }else {
+            return setError('Invalid Password')
+        }
+    }
+
+    const saveUser = (createdUser) => {
+        return usersRef.child(createdUser.user.uid).set({
+            name: createdUser.user.displayName,
+            avatar: createdUser.user.photoURL
+        })
+    }
+
+    return (
+        <Grid textAlign="center" verticalAlign="middle" className="app">
+            <Grid.Column className="register-column">
+                <Header as="h2" textAlign="center">
+                    Sign Up
+                </Header>
+                <Form size="large" onSubmit={handleSubmit}>
+                    <Segment stacked>
+
+                        <Form.Input name="username" icon="user" iconPosition="left" placeholder="Username" onChange={(e) => setUsername(e.target.value)} value={username} type="text" required />
+
+                        <Form.Input name="email" icon="mail" iconPosition="left" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email} type="email" required />
+
+                        <Form.Input name="password" icon="lock" iconPosition="left" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} type="password" required />
+
+                        <Form.Input name="passwordConfirmation" icon="repeat" iconPosition="left" placeholder="Password Confirmation" onChange={(e) => setPasswordConfirmation(e.target.value)} value={passwordConfirmation} type="password" required />
+
+                        <Button disabled={isLoading} className={isLoading ? 'loading' : ''} color="blue" fluid size="large">Register</Button>
+                    </Segment>
+                </Form>
+                {error.length > 0 && <Message error>{error}</Message>}
+                <Message>Already a user? <NavLink to="/login">Login</NavLink> </Message>
+
+            </Grid.Column>
+        </Grid>
+    );
 }
- 
+
 export default Register;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'semantic-ui-css/semantic.min.css';
 import App from './components/App';
-import Login from './components/Auth/Login'
-import Register from './components/Auth/Register'
-import { BrowserRouter, Switch, Route} from 'react-router-dom'
-
-
-const Root = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </BrowserRouter>
-);
-
-
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
