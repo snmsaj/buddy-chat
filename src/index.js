0a5d43f18777feeb72f70f247b43fe73b06cab86
Shawn Saj <76454973+snmsaj@users.noreply.github.com>
2021-05-04 23:16:46 -0400

    Did not setup redux but heavily modified index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import firebase from './firebase';
+import { BrowserRouter, Switch, Route, useHistory, withRouter } from 'react-router-dom';
 
+const Root = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        history.push("/");
+      }
+    })
+  }, [history])
+
+  return (
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Switch>
+  )
+}
+
+const RootWithAuth = withRouter(Root);
 
 ReactDOM.render(
-  <React.StrictMode>
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-      </Switch>
-    </BrowserRouter>
-  </React.StrictMode>,
+      <RootWithAuth />
+    </BrowserRouter>,
   document.getElementById('root')
 );
