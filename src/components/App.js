bae1658b1c006d15d469d4a1775f9e6eb61a2b0b
Shawn Saj <76454973+snmsaj@users.noreply.github.com>
2021-05-06 01:31:21 -0400

    commented out ColorPanel and MetaPanel. May add it back in later.

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,24 @@
 import './App.css';
 import { connect } from 'react-redux';
 import { Grid } from 'semantic-ui-react';
-import ColorPanel from './Layout/ColorPanel';
+// import ColorPanel from './Layout/ColorPanel';
 import SidePanel from './Layout/SidePanel';
 import Messages from './Layout/Messages';
-import MetaPanel from './Layout/MetaPanel';
+// import MetaPanel from './Layout/MetaPanel';
 
 const App = ({currentUser}) => {
   return (
     <Grid columns="equal" className="app">
-      <ColorPanel />
+      {/* <ColorPanel /> */}
       <SidePanel currentUser={currentUser} />
 
       <Grid.Column style={{marginLeft:320}}>
         <Messages />
       </Grid.Column>
 
-      <Grid.Column width={4}>
+      {/* <Grid.Column width={4}>
         <MetaPanel />
-      </Grid.Column>
+      </Grid.Column> */}
     </Grid>
   );
 }
