c07966fda6fd4ec220a9f8ca70f25e62d76328f6
Shawn Saj <76454973+snmsaj@users.noreply.github.com>
2021-05-07 13:03:26 -0400

    firebase deploy

--- src/components/Layout/Messages/MessagesHeader.js
+++ src/components/Layout/Messages/MessagesHeader.js
@@ -1,4 +1,4 @@
-import { Header, Segment, Icon } from 'semantic-ui-react';
+import { Header, Segment } from 'semantic-ui-react';
 
 const MessagesHeader = () => {
     return ( 
@@ -6,9 +6,9 @@ const MessagesHeader = () => {
             <Header fluid="true" as="h2" floated="left" style={{marginBottom: 0}}>
                 <span>
                 Group
-                <Icon name={"star outline"} color="black" />
+                
                 </span>
-                <Header.Subheader>2 Users</Header.Subheader>
+                {/* <Header.Subheader>2 Users</Header.Subheader> */}
             </Header>
         </Segment>
      );
